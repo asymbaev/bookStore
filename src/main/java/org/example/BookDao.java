348f74b0515567cfb15ef92e0b694b3447a11601
Dastan <asymbaevdastan07@gmail.com>
2023-12-18 12:49:17 -0500

    Added DAO interface book

---
+++ src/main/java/org/example/BookDao.java
@@ -0,0 +1,12 @@
+package org.example;
+
+import java.util.List;
+
+public interface BookDao {
+    void addBook(Book book);
+    void updateBook(Book book);
+    void deleteBook(int bookId);
+    Book getBookById(int bookId);
+
+    List<Book> getAllBooks();
+}
